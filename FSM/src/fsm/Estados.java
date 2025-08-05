44820cc323cde123654aab3b81b03b5027073388
DIDIEV <stiventorres608@gmail.com>
2025-08-04 22:22:46 -0500

    Use netBeans to edit

--- FSM/src/fsm/Estados.java
+++ FSM/src/fsm/Estados.java
@@ -9,11 +9,34 @@ import java.util.*;
  * @author Didie
  */
 public class Estados {
-    private Map<String, Transiciones> transiciones;
-    private String entra;  
-    
-    public Estados(String entra) {
+    private String nombre;
+    private Map<String, Estados> transiciones;
+    private Map<String,String> salidas;
+
+    public Estados(String nombre) {
+        this.nombre = nombre;
         this.transiciones = new HashMap<>();
-        this.entra = entra;
+        this.salidas = new HashMap<>();
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void agregarTransicion(String entrada,Estados siguiente,String salida){
+        transiciones.put(entrada,siguiente);
+        salidas.put(entrada,salida);
+    }
+    public Estados siguienteEstado(String entrada){
+        return transiciones.get(entrada);
+    }
+    public String calcularSalida(String entrada){
+        return salidas.get(entrada);
+    }
+    @Override
+    public String toString(){
+        return "estado: " + nombre;
     }
 }
